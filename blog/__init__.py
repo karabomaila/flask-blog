5e3edd196845f3444b58ffb87adf1287ed74da56
Karabo <mailakarabo3@gmail.com>
2024-02-02 12:59:57 +0200

    app config

---
+++ blog/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    app  = Flask(__name__, instance_relative_config=True) # new flask app instance
+
+    # database set up
+    app.config.from_mapping(
+        SECRETE_KEY='Dev',
+        DATABASE=os.path.join(app.instance_path, 'blog.sqlite')
+    )
+
+    if test_config is None: # prod
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # for testing
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def indexPage():
+        return "Blog posts!"
+
+    return app
