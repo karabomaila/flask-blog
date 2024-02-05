ce292684e70727175b92b5246110ade6d5bb3685
Karabo <mailakarabo3@gmail.com>
2024-02-05 08:58:51 +0200

    db file updates

--- blog/db.py
+++ blog/db.py
@@ -1,10 +1,10 @@
 # SQL database set up file
-
 import sqlite3
 import click
 
 from flask import current_app, g
 
+# for getting the database connection.
 def get_db():
     if 'database_connection' not in g:
         g.database_connection = sqlite3.connect(
@@ -22,18 +22,20 @@ def close_db(e=None):
     if db is not None:
         db.close()
 
-# create the tables
+# get the database connection and create the tables.
 def init_db():
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
 
+# creating a command to initialize the database.
 @click.command('init-db')
 def init_db_command():
     init_db()
     click.echo("Initialised the database.")
 
+# adding the close_db method and init_db_command to the app.
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
